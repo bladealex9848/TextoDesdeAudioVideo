700091e5efa7519dd5e8ebe8207e15eefc71a8c4
Alexander Oviedo Fadul <45949794+bladealex9848@users.noreply.github.com>
2024-01-05 14:33:50 -0500

    Actualización de la rama main

--- TranscriptorAV.py
+++ TranscriptorAV.py
@@ -1,5 +1,6 @@
 import os
 import streamlit as st
+from PIL import Image
 import subprocess
 import zipfile
 import io
@@ -7,7 +8,27 @@ import shutil
 import uuid
 import time
 
-# Funciones de internacionalización y manejo de errores
+# Configuración de Streamlit / Streamlit Configuration
+st.set_page_config(
+    page_title="TranscriptorAV: Tu asistente para transcripciones de audio y video",
+    page_icon="🎙️",
+    initial_sidebar_state='collapsed',
+    menu_items={
+        'Get Help': 'https://www.isabellaea.com',  # Actualiza con tu URL de ayuda si es necesario
+        'Report a bug': None,  # Puedes agregar un enlace para reportar errores si lo deseas
+        'About': ("TranscriptorAV es una herramienta avanzada para la transcripción automática "
+                  "de archivos de audio y video. Utiliza modelos de aprendizaje profundo para "
+                  "convertir con precisión contenido multimedia en texto, facilitando la accesibilidad y "
+                  "la comprensión del contenido. Esta herramienta es ideal para periodistas, estudiantes, "
+                  "profesionales de medios y cualquier persona que necesite transcribir contenido multimedia.")
+    }
+)
+
+# Carga y muestra el logo de la aplicación / Load and show the application logo
+logo = Image.open('img/logo.png')
+st.image(logo, width=250)
+
+# Funciones de internacionalización y manejo de errores / Internationalization and error handling functions
 def get_text(text_key, lang):
     texts = {
         "title": {
@@ -62,6 +83,10 @@ def get_text(text_key, lang):
             "es": "Archivo procesado exitosamente.",
             "en": "File successfully processed."
         },
+        "created_by": {
+            "es": "Creado por:",
+            "en": "Created by:"
+        },
         "agent_description": {
             "es": ("TranscriptorAV es tu asistente personalizado para la transcripción automática "
                    "de archivos de audio y video. Utilizando la tecnología de aprendizaje profundo de Whisper, "
@@ -75,6 +100,7 @@ def get_text(text_key, lang):
     }
     return texts[text_key][lang]
 
+# Función para procesar el archivo / Function to process the file
 def process_file(uploaded_file, model_choice, language_code, session_id, lang):
     file_name_without_extension = os.path.splitext(uploaded_file.name)[0]
     temp_dir = f"/tmp/{session_id}"
@@ -105,6 +131,7 @@ def process_file(uploaded_file, model_choice, language_code, session_id, lang):
 
     return get_text("file_processed_success", lang), zip_file_name
 
+# Función para eliminar el directorio temporal / Function to cleanup the temporary directory
 def cleanup(temp_dir, retries=5, delay=1):
     """Intenta eliminar el directorio temporal con reintentos.
 
@@ -125,47 +152,58 @@ def cleanup(temp_dir, retries=5, delay=1):
     else:
         print(f"No se pudo eliminar el directorio temporal {temp_dir} después de {retries} reintentos.")
 
-
+# Configuración de Streamlit / Streamlit configuration
 def set_streamlit_config(lang):
-    st.set_page_config(page_title=get_text("title", lang), initial_sidebar_state='collapsed')
+    #st.set_page_config(page_title=get_text("title", lang), initial_sidebar_state='collapsed')
     st.title(get_text("title", lang))
     st.write(get_text("agent_description", lang))
 
+# Configuración de la barra lateral / Sidebar configuration
 def sidebar_config():
     language_choice = st.sidebar.selectbox(
-        get_text("select_language", st.session_state['lang']),
+        "Select the language / Selecciona el idioma",
         ("Español", "English")
     )
-    lang = 'es' if language_choice == "Español" else 'en'
-    language_code = 'es' if lang == 'es' else 'en'
+    lang = 'en' if language_choice == "English" else 'es'
+    st.session_state['lang'] = lang
 
     model_choice = st.sidebar.selectbox(
         get_text("select_model", lang),
-        ("tiny", "base", "small", "medium", "large")
+        ("small", "base", "tiny")
     )
-    return lang, language_code, model_choice
+    return lang, model_choice
 
+# Función principal / Main function
 def main():
+    # Inicializar las claves de estado de sesión si no existen
+    if 'cleanup_flag' not in st.session_state:
+        st.session_state['cleanup_flag'] = False
+    if 'lang' not in st.session_state:
+        st.session_state['lang'] = 'es'
     if 'session_id' not in st.session_state:
         st.session_state['session_id'] = str(uuid.uuid4())
 
-    if 'cleanup_flag' not in st.session_state:
-        st.session_state['cleanup_flag'] = False
+    # Configuración de la barra lateral para seleccionar el idioma y modelo
+    lang, model_choice = sidebar_config()
 
-    if 'lang' not in st.session_state:
-        st.session_state['lang'] = 'es'
+    # Actualización del título y la descripción según el idioma seleccionado
+    st.title(get_text("title", lang))
+    st.write(get_text("agent_description", lang))
 
-    set_streamlit_config(st.session_state['lang'])
-    lang, language_code, model_choice = sidebar_config()
-    
-    # Agregar footer a la barra lateral
-    st.sidebar.markdown('---')  # Agrega una línea divisoria
-    st.sidebar.subheader('Created by' if lang == 'en' else 'Creado por:')
-    st.sidebar.markdown('Alexander Oviedo Fadul')
+    # Agregar footer a la barra lateral / Add footer to the sidebar
+    st.sidebar.markdown('---')  # Agrega una línea divisoria / Add a divider
+    st.sidebar.subheader(get_text("created_by", lang))
     st.sidebar.markdown(
-        "[GitHub](https://github.com/bladealex9848) | [Website](https://alexander.oviedo.isabellaea.com/) | [Instagram](https://www.instagram.com/alexander.oviedo.fadul) | [Twitter](https://twitter.com/alexanderofadul) | [Facebook](https://www.facebook.com/alexanderof/) | [WhatsApp](https://api.whatsapp.com/send?phone=573015930519&text=Hola%20!Quiero%20conversar%20contigo!%20)"
+        "Alexander Oviedo Fadul\n"
+        "[GitHub](https://github.com/bladealex9848) | "
+        "[Website](https://alexander.oviedo.isabellaea.com/) | "
+        "[Instagram](https://www.instagram.com/alexander.oviedo.fadul) | "
+        "[Twitter](https://twitter.com/alexanderofadul) | "
+        "[Facebook](https://www.facebook.com/alexanderof/) | "
+        "[WhatsApp](https://api.whatsapp.com/send?phone=573015930519&text=Hola%20!Quiero%20conversar%20contigo!%20)"
     )
 
+    # Campo para subir el archivo / Field to upload the file
     st.write(get_text("upload_file", lang))
     uploaded_file = st.file_uploader(
         get_text("choose_file", lang),
@@ -173,11 +211,12 @@ def main():
                "mp4", "mkv", "flv", "avi", "mov", "wmv", "3gp", "webm", "mpg", "m2v", "m4v", "3g2", "asf", "f4v", "mpe", "ts", "vob", "mpeg"],
         accept_multiple_files=False
     )
+
     if uploaded_file:
         st.session_state['cleanup_flag'] = True
         st.write(get_text("file_uploaded", lang), uploaded_file.name)
         if st.button(get_text("process", lang)):
-            message, output_file_or_error = process_file(uploaded_file, model_choice, language_code, st.session_state['session_id'], lang)
+            message, output_file_or_error = process_file(uploaded_file, model_choice, lang, st.session_state['session_id'], lang)
             if message == get_text("file_processed_success", lang):
                 with open(output_file_or_error, 'rb') as f:
                     zip_bytes = io.BytesIO(f.read())
@@ -186,10 +225,12 @@ def main():
                 st.error(f"{message}: {output_file_or_error}")
                 cleanup(os.path.dirname(output_file_or_error))
     else:
+        # Limpieza condicional de archivos temporales
         if st.session_state['cleanup_flag']:
             temp_dir = f"/tmp/{st.session_state['session_id']}"
             cleanup(temp_dir)
             st.session_state['cleanup_flag'] = False
 
+# Ejecutar la aplicación
 if __name__ == "__main__":
     main()
